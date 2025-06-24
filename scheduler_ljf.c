2b4016618abccaffa83b37c8b0548ec489bbd98f
StenioEric <stenioeric@gmail.com>
2025-06-23 22:24:43 -0300

    feat: implementação  dos  5 algoritmos de escalonamento

--- scheduler_ljf.c
+++ scheduler_ljf.c
@@ -1,25 +1,63 @@
 #include <stdio.h>
+#include "queue.h" // Certifique-se de que queue.h contém a definição completa de struct node
+#include "proc.h"
+#include "stats.h"
+#include "utils.h"
 
-#include "queue.h" // contem funções uteis para filas
-#include "proc.h"  // possui as funções dos processos
-#include "stats.h" // possui as funções de estatisticas 
-#include "utils.h" // possui funções uteis 
-
-// Utilizando as variáveis globais definidas no 'main'
-extern struct queue * ready;    // fila de aptos
-extern struct queue * ready2;   // segunda fila de aptos
-extern struct queue * blocked;  // fila de bloqueados
-extern struct queue * finished; // fila de finalizados
-// NOTE: essa fila de finalizados é utilizada apenas para
-// as estatisticas finais
-
-// variavel global que indica o tempo maximo que um processo pode executar ao todo
+// Variáveis globais
+extern struct queue * ready;
+extern struct queue * ready2;
+extern struct queue * blocked;
+extern struct queue * finished;
 extern int MAX_TIME;
 
 struct proc * scheduler(struct proc * current)
 {
-    struct proc * selected; 
+    struct proc * selected = NULL;
 
-    return NULL;
+    if (current != NULL)
+    {
+        switch (current->state)
+        {
+            case READY:
+                enqueue(ready, current);
+                count_ready_in(current);
+                break;
+            case BLOCKED:
+                enqueue(blocked, current);
+                count_blocked_in(current);
+                break;
+            case FINISHED:
+                enqueue(finished, current);
+                count_finished_in(current);
+                break;
+            default:
+                printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+        }
+    }
+
+    if (isempty(ready))
+    {
+        return NULL;
+    }
+
+    // Procurar processo com maior remaining_time na fila 'ready'
+    struct proc * aux = ready->head;
+    selected = aux;
+
+    for (; aux != NULL; aux = aux->next)
+    {
+        if (aux->remaining_time > selected->remaining_time)
+        {
+            selected = aux;
+        }
+    }
+
+    // Remover processo selecionado da fila ready
+    dequeue_bypid(ready, selected->pid);
+
+    count_ready_out(selected);
+    selected->state = RUNNING;
+
+    return selected;
 }
-
